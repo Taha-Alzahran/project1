3e9911945517d971e04e119fc34856531061beb3
Taha Zahran <tahazahran264@gmail.com>
2022-02-10 20:29:01 +0200

    new js

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ function newrate(){
     document.getElementById("ratevalue").innerText=rato;
 }
 function compute(){
-    document.getElementById("result").innerHTML="If you deposit "+amount+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="If you deposit "+amount+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+rateval+",\<br\>in the year "+year+"\<br\>"
 }
